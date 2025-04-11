e42686eda82f08b70a955c3e0a5c15054d09dfe1
e-cornwell <ethan.aubrey22@gmail.com>
2025-04-11 17:13:47 -0600

    Impliment Gold Label, Starting Gold, Gold Gain

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,9 +2,12 @@ extends PathFollow3D
 
 @export var speed: float = 2.5
 @export var max_health := 50
+@export var gold_gain := 15
 
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 var current_health: int:
 	set(health_in):
@@ -12,6 +15,7 @@ var current_health: int:
 			animation_player.play("TakeDamage")
 		current_health = health_in
 		if current_health < 1:
+			bank.gold += gold_gain
 			queue_free()
 	
 
