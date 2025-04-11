cac3a77121584ab76db988f422d974ae68acd5bb
e-cornwell <ethan.aubrey22@gmail.com>
2025-04-10 20:35:41 -0600

    added two new animations

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
 @export var projectile: PackedScene
-@export var turret_range: float = 10.0
+@export var turret_range := 10.0
 
 var enemy_path: Path3D
 var target: PathFollow3D
 
 @onready var cannon_front: MeshInstance3D = $TurretBase/TurretTop/CannonBase/CannonFront
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	target = find_best_target()
@@ -19,13 +20,14 @@ func _on_timer_timeout() -> void:
 		add_child(shot)
 		shot.global_position = cannon_front.global_position
 		shot.direction = global_transform.basis.z
+		animation_player.play("fire")
 
 func find_best_target() -> PathFollow3D:
 	var best_target = null
 	var best_progress = 0
 	for enemy in enemy_path.get_children():
 		if enemy is PathFollow3D:
-			var distance: = global_position.distance_to(enemy.global_position)
+			var distance := global_position.distance_to(enemy.global_position)
 			if distance < turret_range:
 				if enemy.progress > best_progress:
 					best_target = enemy
